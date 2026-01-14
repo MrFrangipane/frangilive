5aae60d03d77755e3d5866078838cef656d3a1b3
MrFrangipane <contact@frangipane.me>
2026-01-14 13:24:05 +0100

    feat: detect Fireface interface (as it's not always the same hw number)

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from pprint import pp
 import subprocess
 import time
+import re
 
 import jack
 import mido
 
 
+_RE = re.compile(r'card ([0-9]+)[^\[]+\[([^\]]+)')
+
+
+print("Detecting Fireface...")
+detected_number = ""
+output = subprocess.check_output(["aplay", "-l"]).decode()
+for result in _RE.findall(output):
+    number, name = result
+    if "fireface" in name.lower():
+        detected_number = number
+        print("Detected interface number " + detected_number)
+        break
+
+if detected_number == "":
+    raise RuntimeError("Could not detect Fireface with aplay -l")
+
+
 print("Starting JACK...")
 subprocess.check_output(["jack_control", "ds", "alsa"])
-subprocess.check_output(["jack_control", "dps", "device", "hw:5"])  # fireface
+subprocess.check_output(["jack_control", "dps", "device", "hw:" + detected_number])
 subprocess.check_output(["jack_control", "start"])
 
 print("Starting Overwitch...")
 subprocess.check_output(["systemctl", "--user", "restart", "overwitch"])
 time.sleep(1)
-print(subprocess.check_output(["systemctl", "--user", "status", "overwitch"]).decode())
+#print(subprocess.check_output(["systemctl", "--user", "status", "overwitch"]).decode())
 
 
 client = jack.Client("Frangilive")
@@ -22,11 +40,11 @@ client.activate()
 
 client.inports.register('Syntakt:Main L')
 
-print("JACK audio ports")
-pp(client.get_ports(is_audio=True))
+#print("JACK audio ports")
+#pp(client.get_ports(is_audio=True))
 
-print("JACK MIDI ports")
-pp(client.get_ports(is_midi=True))
+#print("JACK MIDI ports")
+#pp(client.get_ports(is_midi=True))
 
 try:
     client.connect('Digitone:Main L', 'system:playback_1')
@@ -34,7 +52,7 @@ try:
 
     client.connect('Syntakt:Main L', 'system:playback_1')
     client.connect('Syntakt:Main R', 'system:playback_2')
-    
+
     client.connect('Digitakt:Main L', 'system:playback_1')
     client.connect('Digitakt:Main R', 'system:playback_2')
 
@@ -49,12 +67,12 @@ print("Starting MIDI forwarding...")
 midi_inputs = mido.get_input_names()
 midi_outputs = mido.get_output_names()
 
-print("Available input ports:")
-print("\n".join(midi_inputs))
-print("")
-print("Available output ports:")
-print("\n".join(midi_outputs))
-print("")
+#print("Available input ports:")
+#print("\n".join(midi_inputs))
+#print("")
+#print("Available output ports:")
+#print("\n".join(midi_outputs))
+#print("")
 
 def find_name(name_prefix: str, names: list[str]):
     for name in names:
